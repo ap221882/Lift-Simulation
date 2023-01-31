4bd6428b96eef8c918325e669356cbadc9391502
ajay <ap221882@gmail.com>
2023-01-31 18:36:20 +0530

    feat: created Lift Class

---
+++ src/js/Lift.js
@@ -0,0 +1,18 @@
+class Lift {
+  #level;
+  constructor(level) {
+    this.#level = level;
+  }
+
+  get level() {
+    return this.#level;
+  }
+
+  increaseLevel(value) {
+    this.#level += value;
+  }
+
+  decreaseLevel(value) {
+    this.#level -= value;
+  }
+}
